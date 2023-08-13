349eb9c602aab12729b4de5477a4edd0198fa2d3
iterat1on <101182303+iterat1on@users.noreply.github.com>
2023-08-13 20:12:26 +0900

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,8 +20,8 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "iterat1on",
+  title: "데이터분석가 이호용",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
